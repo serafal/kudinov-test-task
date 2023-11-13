938e7e47b502fed1f394ce69ac368a5a1fd82850
makar <gamemakarena@gmail.com>
2023-11-13 16:06:31 +0300

    Базовая вёрстка готова

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Header.css"
 import HeaderBanner from "../../data/imgs/Banner image.png"
+import AvatarImg from "../../data/imgs/Avatar profile.png"
+import MesageIcon from "../../data/other/Icon.png"
+import PhoneIcon from "../../data/other/Phone.png"
 
 
 export default function Header() {
@@ -10,6 +13,16 @@ export default function Header() {
   return (
   <div className="Header">
     <img src={HeaderBanner} className="HeaderBanner"></img>
+    <div className="Avatar">
+      <img src={AvatarImg} alt="" className="AvatarImg"/>
+      <div className="AvatarNameBox">
+        <h3 className="AvatarName">Ricardo Cooper</h3>
+      </div>
+      <div className="headerButtons">
+        <button><img src={MesageIcon} className="Button-Mesage"alt=""/>    Mesage</button>
+        <button><img src={PhoneIcon} className="Button-Call" alt="" />    Call</button>
+      </div>
+    </div>
   </div>
   );
 }
